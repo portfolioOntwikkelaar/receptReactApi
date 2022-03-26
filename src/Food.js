f82c97935ba55675352c27d604a0646fb4af80af
noordseoostende <voropayev1@mail.ru>
2022-03-26 17:27:00 +0100

    100

--- src/Food.js
+++ src/Food.js
@@ -32,35 +32,48 @@ const Food = () => {
       <div className="header">
         <nav className='nav'>
           <div className="logo">
-            <img src={cake} alt="" />
+            <img src={cake} alt="" className='img-1' />
           </div>
-          <ul>
+          <ul className='ul'>
             <li><a href="#">Sponsors</a></li>
             <li><a href="#">Klanten</a></li>
             <li><a href="#">Maaltijd</a></li>
             <li><a href="#">Contact</a></li>
           </ul>
 
-        </nav>
-      </div>
       <form className='form_1' onSubmit={submitForm}>
         <input type="text" placeholder='schrijf maar benaming van het artikel' value={query} onChange={e => setQuery(e.target.value)} />
-        <button type='submit'>Invoeren</button>
+        <button className="button-3" type='submit'>Invoeren</button>
       </form>
+        </nav>
+      
 
       <div>
+
+        <div className='text'>
+          <h1><span className='span-h2'>Snelste Levering </span>Ter Wereld</h1>
+          <p>Voel Het <span>Smaak</span></p>
+        </div>
+        <div className='print'>
+
         {rcp.map((item,index) => {
           return (
-            <div key={index}>
-              <img src={item['recipe']['image']} alt="" />
-              <p>{item['recipe']['label']}</p>
-              <p>{item['recipe']['calories'].toFixed(2)}</p>
-              <p>{<a href={item['recipe']['url']}>Lezen</a>}</p>
-              {/* <p>{item['recipe']['mealType']}</p> */}
+            <div key={index} className='product'>
+              <img src={item['recipe']['image']} alt=""  />
+              <p><span>Benaming : </span>{item['recipe']['label']}</p>
+              <p><span>Calorie : </span>{item['recipe']['calories'].toFixed(2)}</p>
+              <div className='buttons'>
+
+              <p>{<a className='d-2' href={item['recipe']['url']}>Lezen</a>}</p>
+              <button className='button-2'>Bestelling</button>
+              </div>
+              
             </div>
           )
         })}
       </div>
+        </div>
+        </div>
     </div>
   )
 }
